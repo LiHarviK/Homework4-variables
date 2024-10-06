609685578934e21e4724f2126f768991ff51e56d
LiHarviK <steve2002minecraft@gmail.com>
2024-10-06 22:08:40 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+completed_hw = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = completed_hw / hours_spent
+
+print('Курс:', course_name, ', всего задач:', completed_hw, ', среднее время выполнения:', time_per_task / 100, 'часа')
